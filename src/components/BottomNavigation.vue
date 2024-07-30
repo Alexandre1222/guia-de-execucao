64aacc2149a60aff674ce433f62333f8a78d3219
Alexandre Rodrigues <alexandre.ro1222@gmail.com>
2024-07-30 00:50:27 -0300

    feat: adição de imagem nova para em construção, adição de v-bottom-navigation

---
+++ src/components/BottomNavigation.vue
@@ -0,0 +1,26 @@
+<script setup>
+import router from '@/router';
+</script>
+<template>
+<v-bottom-navigation grow bg-color="primary">
+  <v-btn @click.stop="router.push('/login')">
+    <v-icon>mdi-home</v-icon>
+
+    <span>Inicio</span>
+  </v-btn>
+
+  <v-btn value="favorites">
+    <v-icon>mdi-heart</v-icon>
+
+    <span>Favorites</span>
+  </v-btn>
+
+  <v-btn value="nearby">
+    <v-icon>mdi-map-marker</v-icon>
+
+    <span>Nearby</span>
+  </v-btn>
+</v-bottom-navigation>
+</template>
+<style scoped>
+</style>
